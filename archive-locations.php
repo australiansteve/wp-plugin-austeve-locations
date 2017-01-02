9124ff23ab15f8ceb6c07fdd38e03963a0f96e12
Steve weaver Crawford <steve@australiansteve.com>
2017-01-02 15:42:41 -0400

    Initial commit of Locations plugin - copy/pasted from Events

---
+++ archive-locations.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying archive pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package AUSteve Locations
+ */
+
+get_header(); ?>
+
+<div class="row"><!-- .row start -->
+
+	<div class="small-12 columns"><!-- .columns start -->
+
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+
+			<?php if ( have_posts() ) : ?>
+
+				<header class="page-header">
+					<?php
+						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_description( '<div class="taxonomy-description">', '</div>' );
+					?>
+				</header><!-- .page-header -->
+
+				<div class="row small-up-1 medium-up-2 large-up-3 align-middle" id="locations-block-grid">
+
+				<?php /* Start the Loop */ ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<div class="column">
+				<?php 
+
+            		if (locate_template('page-templates/partials/locations-archive.php') != '') {
+						// yep, load the page template
+						get_template_part('page-templates/partials/locations', 'archive');
+					} else {
+						// nope, load the default
+						include( plugin_dir_path( __FILE__ ) . 'page-templates/partials/locations-archive.php');
+					}
+
+				?>
+					</div>
+				<?php endwhile; ?>
+
+				</div> <!-- #locations-block-grid -->
+
+				<?php the_posts_navigation(); ?>
+
+			<?php else : ?>
+
+				<?php get_template_part( 'page-templates/partials/content', 'none' ); ?>
+
+			<?php endif; ?>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+	</div><!-- .columns end -->
+
+</div><!-- .row end -->
+
+<?php get_footer(); ?>
